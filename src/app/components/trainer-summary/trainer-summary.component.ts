7e26edb94ce6b87a81b8bc011c06a8dc2516db18
M-sahin <murat.sa25@gmail.com>
2022-03-31 16:53:39 +0200

    feat: added some elements to summar

--- src/app/components/trainer-summary/trainer-summary.component.ts
+++ src/app/components/trainer-summary/trainer-summary.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Pokemon } from 'src/app/models/pokemon.model';
 
 @Component({
   selector: 'app-trainer-summary',
   templateUrl: './trainer-summary.component.html',
   styleUrls: ['./trainer-summary.component.css']
 })
-export class TrainerSummaryComponent implements OnInit {
+export class TrainerSummaryComponent {
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
+  // getter function for template
+  // returns the users's username
+  get username(): string {
+      return this.userService.user.username;
   }
-
 }
